5c4566a0538e6f9b9a78dc9fd342558aa3bdf178
Yogesh Singh <yogesh.singh@venaai.com>
2026-01-20 15:22:21 +0530

    back to barebones

--- launch_chat_builder.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-Launch Chat-based Configuration Builder
-"""
-
-if __name__ == "__main__":
-    from src.configurable_agents.ui.chat_builder_app import launch_chat_builder
-    launch_chat_builder()
